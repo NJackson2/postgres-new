12c4ff0523d4d24fab86d6137c67261dc5edb045
Nick Jackson <nickdjackson@msn.com>
2022-10-19 17:00:38 -0400

    third

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 import express from 'express'
-import pg from 'pg'
-import { credentials } from './credentials.js'
 
-const { Pool } = pg
+import { getAllCustomers, getCustomerById, addNewCustomer } from './src/customers.js'
 
 const app = express()
+app.use(express.json())
 
-
-app.get('/customers', async (req, res) => {
-    const pool = new Pool(credentials)
-    const customers = await pool.query("SELECT * FROM customers", )
-        .catch(err => res.status(500).send(err))
-    res.send(customers.rows)
-    pool.end()
-})
-
-
+app.get('/customers', getAllCustomers)
+app.get('/customers/:customerId', getCustomerById)
+app.post('/customers', addNewCustomer)
 // some developers sent up a separate file for their routes (e.g., routes.js)
 
 app.listen(3030, () => console.log('Listening on http://localhost:3030...'))
---
+++ src/customers.js
@@ -0,0 +1,32 @@
+import pg from 'pg'
+import { credentials } from '../credentials.js'
+
+const { Pool } = pg
+
+
+export async function getAllCustomers (req, res) {
+    const pool = new Pool(credentials)
+    const customers = await pool.query("SELECT * FROM customers")
+        .catch(err => res.status(500).send(err))
+    res.send(customers.rows)
+    pool.end()
+}
+
+export async function getCustomerById (req, res) {
+    const { customerId } = req.params
+    const pool = new Pool(credentials)
+    const customers = await pool.query(`SELECT * FROM customers WHERE customer_id = ${customerId}`)
+        .catch(err => res.status(500).send(err))
+    res.send(customers.rows)
+    pool.end()
+}
+
+export async function addNewCustomer (req, res) {
+    const { first_name, last_name, email, phone } = req.body
+    const pool = new Pool(credentials)
+    const query = `INSERT INTO customers (first_name, last_name, phone, email) VALUES ('${first_name}', '${last_name}', '${phone}', '${email}')`
+    const customers = await pool.query("SELECT * FROM customers")
+        .catch(err => res.status(500).send(err))
+    res.send(customers.rows)
+    pool.end()
+}
